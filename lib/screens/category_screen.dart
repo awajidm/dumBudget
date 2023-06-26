4089247777e5914ea6c35e6a67a756a30dc2ba70
Abdul <awajidm@outlook.com>
2023-06-26 06:22:58 +0500

    added custom radial_painter to draw circle and list of items (completed for now)

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/helpers/color+helper.dart';
 import 'package:flutter_budget_ui/models/category_model.dart';
+import 'package:flutter_budget_ui/models/expense_model.dart';
+import 'package:flutter_budget_ui/widgets/radial_painter.dart';
 
 class CategoryScreen extends StatefulWidget {
   final Category category;
@@ -9,8 +12,63 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenseList = [];
+    widget.category.expenses.forEach((Expense expense) {
+      expenseList.add(Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        height: 80.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                expense.name,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '\$${expense.cost.toStringAsFixed(2)}',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ));
+    });
+    return Column(children: expenseList);
+  }
+
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach(
+      (Expense expense) {
+        totalAmountSpent += expense.cost;
+      },
+    );
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -43,7 +101,25 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   ),
                 ],
               ),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey[200],
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0,
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
             ),
+            _buildExpenses(),
           ],
         ),
       ),
